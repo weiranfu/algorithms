0fff4fb405c8cf1ff593d60f7e871b5e8808df69
aranne <fuweiran816@gmail.com>
2019-06-12 15:09:55 +0800

    add a method drawTriangle

--- Draw/Drawing_a_Triangle/DrawTriangle.java
+++ Draw/Drawing_a_Triangle/DrawTriangle.java
@@ -1,8 +1,8 @@
 /** Drawing a triangle. */
 public class DrawTriangle {
-    public static void main(String[] args) {
+    public static void drawTriangle(int N) {
         int row = 0;
-        int SIZE = 5;
+        int SIZE = N;
         while (row < SIZE) {
             int col = 0;
             while (col < row) {
@@ -12,5 +12,8 @@ public class DrawTriangle {
             System.out.println("*");
             row = row + 1;
         }
+    }
+    public static void main(String[] args) {
+        drawTriangle(10);    
     }
 }
