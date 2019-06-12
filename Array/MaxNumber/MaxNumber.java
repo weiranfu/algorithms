e8b4e0208d8892249347e5a254df25a2450290be
aranne <fuweiran816@gmail.com>
2019-06-12 17:01:44 +0800

    change s to sum

--- Array/MaxNumber/MaxNumber.java
+++ Array/MaxNumber/MaxNumber.java
@@ -2,15 +2,15 @@ public class MaxNumber {
     /** Returns the maximum value from Array m */
     public static int max(int[] m) {
         int k = 0;
-        int s = 0;
+        int sum = 0;
         int length = m.length;
         while (k < length) {
-            if (s < m[k]) {
-                s = m[k];
+            if (sum < m[k]) {
+                sum = m[k];
             }
             k = k + 1;
         }
-        return s;
+        return sum;
     }
     public static void main(String[] args) {
         int[] numbers = new int[]{9, 2, 15, 2, 22, 10, 6};
