86b12f14120b8828a234a2fbf81f6a31aec4cb5f
aranne <fuweiran816@gmail.com>
2019-06-12 14:45:13 +0800

    Draw a Triangle

---
+++ Draw/Drawing_a_Triangle/DrawTriangle.java
@@ -0,0 +1,16 @@
+/** Drawing a triangle. */
+public class DrawTriangle {
+    public static void main(String[] args) {
+        int row = 0;
+        int SIZE = 5;
+        while (row < SIZE) {
+            int col = 0;
+            while (col < row) {
+                System.out.print("*");
+                col = col + 1;
+            }
+            System.out.println("*");
+            row = row + 1;
+        }
+    }
+}
