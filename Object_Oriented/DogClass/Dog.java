757f0b22f3e52ec46255dcb7d954fa42b32afd58
aranne <fuweiran816@gmail.com>
2019-06-14 16:41:03 +0800

    make a constructor.

--- Object_Oriented/DogClass/Dog.java
+++ Object_Oriented/DogClass/Dog.java
@@ -1,6 +1,11 @@
 public class Dog {
     public int weightInPounds;
 
+    // Dog Constructor.
+    public Dog(int w) {
+        weightInPounds = w;
+    }
+    
     public void makeNoise() {
         if (weightInPounds < 10) {
             System.out.println("Yel!");
--- Object_Oriented/DogClass/DogLauncher.java
+++ Object_Oriented/DogClass/DogLauncher.java
@@ -1,7 +1,6 @@
 public class DogLauncher {
     public static void main(String[] args) {
-        Dog d = new Dog();
-        d.weightInPounds = 25;
+        Dog d = new Dog(25);
         d.makeNoise();
     }
 }
