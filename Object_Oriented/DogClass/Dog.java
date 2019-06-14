38707457820d45b083ea48d258d6bf096f50a82b
aranne <fuweiran816@gmail.com>
2019-06-14 17:48:50 +0800

    add method to compare dogs to make bigger one to bark!

--- Object_Oriented/DogClass/Dog.java
+++ Object_Oriented/DogClass/Dog.java
@@ -15,4 +15,19 @@ public class Dog {
             System.out.println("Woooooof!");
         }
     }
+
+    /** Compare the weight of two dogs. */
+    public static Dog maxDog(Dog d1, Dog d2) {
+        if (d1.weightInPounds > d2.weightInPounds) {
+            return d1;
+        }
+        return d2;
+    }
+    //Non-static maxDog method.
+    public Dog maxDog(Dog d2) {
+        if (this.weightInPounds > d2.weightInPounds) {
+            return this;
+        }
+        return d2;
+    }
 }
