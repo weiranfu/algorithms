e613515ae5073ae5b34080a18e9022d2f55f4f17
aranne <fuweiran816@gmail.com>
2019-06-17 16:24:48 +0800

    add size() get() method.

--- Linked_List/IntList/IntList.java
+++ Linked_List/IntList/IntList.java
@@ -4,9 +4,30 @@
 public class IntList {
 	public int first;
 	public IntList rest;
-
+    
+    // This looks like linked list. 
 	public IntList(Int f, IntList r) {
 		first = f;
 		rest = r;
 	}
+
+    /** Return the size of a list. */
+    public int size() {
+    	if (this.rest == null) {
+    		return 1;
+    	}
+    	return 1 + this.rest.size();
+    }
+
+    /** Get the ith item of the list. */
+    public int get(int i) {
+    	if (i == 0) {
+    		return this.first;
+    	} else {
+    		// this can't be reassigned. copy this to p.
+    		IntList p = this;
+    		p = p.rest;
+    		return get(i - 1);
+    	}
+    }
 }
