048efdc84f22eff24120cfc19823ed1763afa2f5
aranne <fuweiran816@gmail.com>
2019-06-21 22:44:38 +0800

    add size() and addLast(x) methods.

--- Linked_List/IntList/SLList.java
+++ Linked_List/IntList/SLList.java
@@ -12,6 +12,7 @@ public class SLList {
 			item = i;
 			next = n;
 		}
+	}	
 
 	private IntNode first;	
 
@@ -30,11 +31,34 @@ public class SLList {
 		return first.item;
 	}
 
+	/** Adds a new item to the last of this linked list. */
+	public void addLast(int x) {
+		IntNode ptn = first;
+		while (ptn.next != null) {
+			ptn = ptn.next;
+		}
+		ptn.next = new IntNode(x, null); 
+	}
+
+	/** Returns the size of this linked list. */
+	public int size() {
+		return size(first);
+	}
+
+	/** A helper method of size(), returns the size of a IntNode List. */
+	public static int size(IntNode L) {
+		if (L.next == null) {
+			return 1;
+		}
+		return 1 + size(L.next);
+	}
+
 	public static void main(String[] args) {
 		/* Create a list of one integer. */
 		SLList L = new SLList(15);
 		L.addFirst(10);
 		L.addFirst(5);
 		System.out.println(L.getFirst());
+		System.out.println(L.size());
 	}
 }
