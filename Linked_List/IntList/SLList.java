02e7b99fd0a6157de0145cd2245f4ab425e6058b
aranne <fuweiran816@gmail.com>
2019-06-22 00:36:59 +0800

    use sentinel to deal with empty list.

--- Linked_List/IntList/SLList.java
+++ Linked_List/IntList/SLList.java
@@ -14,45 +14,50 @@ public class SLList {
 		}
 	}	
 
-	private IntNode first;	
+	private IntNode sentinel;	
 	private int size;   // cache the size of the list.
 
 	// Creates an empty SLList.
 	public SLList() {
-		first = null;
+		sentinel = new IntNode(816, null);
 		size = 0;
 	}
 	// use InNode class to instantiate.
 	public SLList(int x) {
-		first = new IntNode(x, null);  // This is a pointer.
+		IntNode p = new IntNode(x, null);
+		sentinel = new IntNode(816, p);  // This is a pointer.
 		size = 1;
 	}
 
 	/** Adds a new item to the front of this linked list. */
 	public void addFirst(int x) {
-		first = new IntNode(x, first);
 		size += 1;
+		IntNode p = new IntNode(x, sentinel.next);
+		sentinel = new IntNode(816, p);
 	}
 
 		/** Adds a new item to the last of this linked list. */
 	public void addLast(int x) {
-		IntNode p = first;
+	    size += 1;
+		IntNode p = sentinel;
 		// move p until reaches the end of the list.
 		while (p.next != null) {
 			p = p.next;
 		}
 		p.next = new IntNode(x, null); 
-		size += 1;
 	}
 
 	/** Returns the first item in this linked list. */
 	public int getFirst() {
-		return first.item;
+		if (sentinel.next == null) {
+			System.out.println("THis is an empty list!");
+		}
+		return sentinel.next.item;
 	}
 
 	/** Returns the size of this linked list. */
 	public int size() {
-		return size(first);
+		return size(sentinel.next);
 	}
 
 	/** A helper method of size(), returns the size of a IntNode List. */
@@ -70,7 +75,7 @@ public class SLList {
     */
 	public static void main(String[] args) {
 		/* Create a list of one integer. */
-		SLList L = new SLList(15);
+		SLList L = new SLList();
 		L.addLast(20);
 		L.addFirst(10);
 		L.addFirst(5);
