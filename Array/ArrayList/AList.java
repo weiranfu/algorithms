b99a7eb811a71e4e5cc423d14ae9bbe5b19c8a83
aranne <fuweiran816@gmail.com>
2019-07-06 10:11:36 +0800

    make AList generic.

--- Array/ArrayList/AList.java
+++ Array/ArrayList/AList.java
@@ -8,26 +8,26 @@
  The last item in the list is always in position size - 1.
 */
 
-public class AList {
-	private int[] items;
+public class AList<Item> {
+	private Item[] items;
 	private int size;
 	private static final int RFactor = 2; // Resizing factor: every time we resize the array, we build a 2 times size of the array.
 
 	/** Creates an empty list. */
 	public AList() {
-		items = new int[100];
+		items = (Item []) new Object[100];
 		size = 0;
 	}
 
     /** Resize the underlying array to the target capacity. */
     private void resize(int capacity) {
-		int[] a = new int[capacity];
+		Item[] a = (Item [])new Object[capacity];
 		System.arraycopy(items, 0, a, 0, size);
 		items = a;
 	}
 
     /** Inserts X into the back of this list. */
-    public void addLast(int x) {
+    public void addLast(Item x) {
     	if (size == items.length) {
     		resize(size * RFactor);
     	}
@@ -36,17 +36,17 @@ public class AList {
     }
 
     /** Returns the item from the back of this list. */
-    public int getLast() {
+    public Item getLast() {
     	return items[size - 1];
     }
 
     /** Returns the ith item in this list. */
-    public int get(int i) {
+    public Item get(int i) {
     	if (i < size) {
     		return items[i];
     	}
-    	System.out.println("The list only has " + (size - 1) + " items.");
-    	return 0;
+    	System.out.println("The list only has " + size + " items.");
+    	return null;
     }
 
     /** Returns the number of items in this list. */
@@ -56,11 +56,11 @@ public class AList {
 
     /** Deletes the item from the back of this list and 
       * returns deleted item. */
-    public int removeLast() {
+    public Item removeLast() {
     	if ((double)size / items.length <= 0.25) {
     		resize(items.length / 2);
 		}
-    	int lastItem = getLast();
+    	Item lastItem = getLast();
     	size -= 1;
     	return lastItem;
     }
