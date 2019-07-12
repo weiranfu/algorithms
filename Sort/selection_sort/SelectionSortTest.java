7332c481d4e7df359b0f1ad2c92f33c72acbce69
aranne <fuweiran816@gmail.com>
2019-07-12 22:27:51 +0800

    test finished.

--- Sort/selection_sort/SelectionSortTest.java
+++ Sort/selection_sort/SelectionSortTest.java
@@ -1,36 +1,43 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
 public class SelectionSortTest {
 
     /** Test SelectionSort(). */
-    public void SelectionSortTest() {
+    @Test
+    public void testSelectionSort() {
         String[] input = {"i", "have", "an", "egg"};
         String[] expected = {"an", "egg", "have", "i"};
         SelectionSort.sort(input);
-        org.junit.Assert.assertArrayEquals(input, expected);
+        assertArrayEquals(input, expected);
     }
 
     /** Test findSmallest(). */
-    public void findSmallestTest() {
+    @Test
+    public void testFindSmallest() {
         String[] input = {"i", "have", "an", "egg"};
         int expected = 2;
 
         int actual = SelectionSort.findSmallest(input, 0);
-        org.junit.Assert.assertEquals(expected, actual);
+        assertEquals(expected, actual);
 
         String[] input2 = {"there", "are", "many", "pigs"};
         int expected2 = 2;
 
         int actual2 = SelectionSort.findSmallest(input2, 2);
-        org.junit.Assert.assertEquals(expected2, actual2);
+        assertEquals(expected2, actual2);
     }
 
     /** Test swap(). */
-    public void swapTest() {
+    @Test
+    public void testSwap() {
         String[] input = {"i", "have", "an", "egg"};
         int a = 0;
         int b = 2;
         String[] expected = {"an", "have", "i", "egg"};
 
         SelectionSort.swap(input, a, b);
-        org.junit.Assert.assertArrayEquals(expected, input);
+        assertArrayEquals(expected, input);
     }
 }
