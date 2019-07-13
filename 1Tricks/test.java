b30506700df6a2549322c55dd2a4110606744067
aranne <fuweiran816@gmail.com>
2019-07-13 16:41:10 +0800

    test

---
+++ 1Tricks/test.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import org.junit.Assert.*;
+
+assertEquals(a, b);
+
+assertArrayEquals(array1, array2);
+
+assertTrue(boolean);
+assertFalse(boolean);
+
+assertNotNull(a);
+
+fail();
