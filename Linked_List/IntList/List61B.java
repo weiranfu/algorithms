84ceea3a45dafee7a15e64d8afe252087f4a2180
aranne <fuweiran816@gmail.com>
2019-07-18 00:16:09 +0800

    implement inheritance

--- Linked_List/IntList/List61B.java
+++ Linked_List/IntList/List61B.java
@@ -16,4 +16,12 @@ public interface List61B<Item> {
 
 	/** Returns the size of this list. */
 	public int size();
+
+	/** Prints out the entire list. */
+	default public void print() {
+		for (int i = 0; i < size(); i += 1) {
+			System.out.println(get(i) + " ");
+		}
+		System.out.println();
+	}
 }
