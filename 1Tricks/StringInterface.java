57081d055c99bd6865b5cee84e6facc6efb65da0
aranne <fuweiran816@gmail.com>
2019-07-17 19:05:19 +0800

    string interface

---
+++ 1Tricks/StringInterface.java
@@ -0,0 +1,9 @@
+String a, b;
+
+// Get the length of a string.
+length = a.length();
+
+// Compare two strings.
+a.compareTo(b) < 0;   // If a < b:
+a.compareTo(b) == 0;  // If a == b:
+a.compareTo(b) > 0;   // if a > b:
--- 1Tricks/compareString.java
+++
@@ -1,7 +0,0 @@
-// Compare two strings a and b.
-// If a < b:
-a.compareTo(b) < 0;
-// If a == b:
-a.compareTo(b) == 0;
-// if a > b:
-a.compareTo(b) > 0;
