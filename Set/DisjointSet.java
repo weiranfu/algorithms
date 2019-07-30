a924894e57cbbd6230008f535b614caf09d43581
aranne <fuweiran816@gmail.com>
2019-07-30 22:24:41 +0800

    new disjointSet

---
+++ Set/DisjointSet.java
@@ -0,0 +1,12 @@
+/**
+ * A Disjoint-Sets (or Union-Find) data structure that
+ * keeps track of a fixed number of elements partitioned into a number of disjoint sets.
+ */
+
+public interface DisjointSet {
+     /** Connect x and y. Also known as union. */
+     void connect(int x, int y);
+
+     /** Checks to see if two items are connected. */
+     boolean isConnected(int x, int y);
+}
