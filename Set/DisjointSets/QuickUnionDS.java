4ab28078d9187698ea858ec8a0430fc7109b9e78
aranne <fuweiran816@gmail.com>
2019-07-31 20:23:09 +0800

    weighted disjoint set with path compression

--- Set/DisjointSets/QuickUnionDS.java
+++ Set/DisjointSets/QuickUnionDS.java
@@ -10,16 +10,16 @@ public class QuickUnionDS implements DisjointSet {
     public QuickUnionDS(int N) {
         parent = new int[N];
         for (int i = 0; i < N; i += 1) {
-            parent[i] = i;
+            parent[i] = -1;     // Each value is -1, because they're all roots.
         }
     }
 
-    /** Returns the parent of an item in a set. */
+    /** Returns the root of an item in a set. */
     private int find(int x) {
-        if (parent[x] < 0) {
-            return x;
+        while (parent[x] >= 0) {
+            x = parent[x];
         }
-        return find(parent[x]);
+        return x;
     }
 
     /** Connect two disjoint sets together. */
@@ -27,14 +27,12 @@ public class QuickUnionDS implements DisjointSet {
     public void connect(int x, int y) {
         int xp = find(x);
         int yp = find(y);
-        if (parent[xp] != parent[yp]) {
-            parent[xp] = yp;      // Connect two roots.
-        }
+        parent[xp] = yp;      // Connect two roots.
     }
 
     /** Returns true if two sets are connected. */
     @Override
     public boolean isConnected(int x, int y) {
-        return parent[x] == parent[y];
+        return find(x) == find(y);
     }
 }
