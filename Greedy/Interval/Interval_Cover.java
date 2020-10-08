61d8155275706a46c651ea7bb67f3ced68b2447d
Weiran Fu <fuweiran816@gmail.com>
2020-10-07 21:00:17 -0400

    add solution2 [jump game]

--- Greedy/Interval/Interval_Cover.java
+++ Greedy/Interval/Interval_Cover.java
@@ -10,41 +10,61 @@ import java.util.Arrays;
  * 算法思路：
  * 将所有区间按 左端点 从小到大排序
  * 从前往后依次枚举每个区间，在所有能覆盖 start 的区间中选择 右端点 最大的，然后将 start 更新成右端点的最大值
+ * O(nlogn)
  */
 public class Interval_Cover {
+    int INF = 0x3f3f3f3f;
 
     public int minCover(int[][] ranges, int start, int end) {
         int n = ranges.length;
-        int INF = 0x3f3f3f3f;
-
         Arrays.sort(ranges, (a, b) -> a[0] - b[0]);
 
         int cnt = 0;
-        boolean success = false;
         for (int i = 0; i < n; i++) {
             int j = i, r = -INF;
             while (j < n && ranges[j][0] <= start) {
                 r = Math.max(r, ranges[j][1]);
                 j++;
             }
-
-            cnt++;                      // use an max right end interval
-
-            if (r < start) {            // if cannot reach start interval
-                cnt = -1;
-                break;
-            }
-
-            if (r >= end) {             // cover all interval
-                success = true;
-                break;
-            }
+            cnt++;                      // choose an interval with max right boundary
+            if (r <= start) return -1;  // if cannot reach start interval
+            if (r >= end) return cnt;   // cover all interval
 
             start = r;                  // update start
             i = j - 1;                  // update i to j - 1
         }
 
-        return success ? cnt : -1;      // it is possible that there's not enough ranges to cover whole interval after for loop.
+        return -1;      // it is possible that there's not enough ranges to cover whole interval after for loop.
+    }
+
+    /**
+     * 方法2: 不用 sort 区间
+     * 如果 [start, end] 区间不是特别大的情况下，转化为 jump game problem
+     * [start, end] 里每一个点有一个最远能覆盖的距离，即 jump ability
+     * 然后从前往后依次扫描每个点，在当前 end 区间内找的最远的能覆盖的点，更新end
+     * O(n)
+     */
+    public int minCover2(int[][] ranges, int start, int end) {
+        int L = end - start + 1;
+        int[] jump = new int[L];
+        for (int[] range : ranges) {
+            int l = Math.max(start, range[0]) - start;      // left boundary
+            int r = Math.min(end, range[1]) - start;        // right boundary
+            jump[l] = Math.max(jump[l], r);                 // update jump ability
+        }
+        int cnt = 0, curr = 0;
+        for (int i = 0; i < L; i++) {                       // 扫描 [0, L-1] 中的每一个点 即 [start, end] 中每一个点
+            int j = i, e = -INF;
+            while (j < L && j <= curr) {
+                e = Math.max(e, jump[j++]);
+            }
+            cnt++;
+            if (e <= curr) return -1;            // cannot reach curr
+            if (e >= L - 1) return cnt;          // cover [0，L-1]
+            curr = e;
+            i = j - 1;
+        }
+        return -1;
     }
 
 }
